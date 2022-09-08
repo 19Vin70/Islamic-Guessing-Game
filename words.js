0b43eda6b92dc6b284865810769c229101cabf43
Marvin Saik <113106676+19Vin70@users.noreply.github.com>
2022-09-09 06:59:13 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,178 @@
+let wordList = [
+    {
+        word: "islam",
+        clue: "Fastest growing religion in the World"
+    },
+    {
+        word: "allah",
+        clue: "The one.and only God"
+    },
+    {
+        word: "muhammad",
+        clue: "The last prophet/messenger of Allah"
+    },
+    {
+        word: "muslim",
+        clue: "Who's religion is Islam"
+    },
+    {
+        word: "salah",
+        clue: "The key of Paradise"
+    },
+    {
+        word: "aqidah",
+        clue: "The foundation of Religion"
+    },
+    {
+        word: "akhlaq",
+        clue: "Refers to Characteristic"
+    },
+    {
+        word: "tajweed",
+        clue: "Correct pronounciation of reading Qur'an"
+    },
+    {
+        word: "quran",
+        clue: "The Holy book of Islam"
+    },
+    {
+        word: "sahaba",
+        clue: "The companions of Prophet Muhammad"
+    },
+    {
+        word: "khadijah",
+        clue: "athe first wife of prophet Muhammad"
+    },
+    {
+        word: "aisha",
+        clue: "The youngest wife of.prophet Muhammad"
+    },
+    {
+        word: "fatimah",
+        clue: "Wife.of Ali"
+    },
+    {
+        word: "ibrahim",
+        clue: "Friend of Allah"
+    },
+    {
+        word: "ismail",
+        clue: "The son of Ibrahim"
+    },
+    {
+        word: "hajar",
+        clue: "Mother of Ismail"
+    },
+    {
+        word: "abdullah",
+        clue: "Father of prophet Muhammad"
+    },
+    {
+        word: "amina",
+        clue: "Mother of prophet Muhammad"
+    },
+    {
+        word: "abutalib",
+        clue: "Uncle of prophet Muhammad"
+    },
+    {
+        word: "abubakar",
+        clue: "The first Khalifah of Islam"
+    },
+    {
+        word: "omar",
+        clue: "Second Khalifah of Islam"
+    },
+    {
+        word: "uthman",
+        clue: "Third Khalifah of Islam"
+    },
+    {
+        word: "ali",
+        clue: "Fourth Khalifah of Islam"
+    },
+    {
+        word: "zakah",
+        clue: "Giving charity"
+    },
+    {
+        word: "nikah",
+        clue: "Marriage in Islam"
+    },
+    {
+        word: "wali",
+        clue: "Guardian of the girl in Nikah"
+    },
+    {
+        word: "tayammum",
+        clue: "Ablution without water"
+    },
+    {
+        word: "wuduh",
+        clue: "Ablution before Salah"
+    },
+    {
+        word: "jibril",
+        clue: "Angel who came to prophet Muhammad"
+    },
+    {
+        word: "israfil",
+        clue: "Angel who holds the trumpet"
+    },
+    {
+        word: "allah",
+        clue: "King of all kings"
+    },
+    {
+        word: "shahada",
+        clue: "Believing in the oneness of Allah"
+    },
+    {
+        word: "hajj",
+        clue: "Pilgrimage to Makkah"
+    },
+    {
+        word: "madina",
+        clue: "The city of prophet Muhammad"
+    },
+    {
+        word: "masjid",
+        clue: "Where the prayer is performed"
+    },
+    {
+        word: "mariam",
+        clue: "Mother of Jesus/Isa"
+    },
+    {
+        word: "ishaq",
+        clue: "Another son of Ibrahim"
+    },
+    {
+        word: "zamzam",
+        clue: "The holy water"
+    },
+    {
+        word: "siyra",
+        clue: "History of prophet"
+    },
+    {
+        word: "abdulmuttalib",
+        clue: "Grandfather of prophet Muhammad"
+    },
+    {
+        word: "makkah",
+        clue: "Where the prophet Muhammad was born"
+    },
+    {
+        word: "halima",
+        clue: "Second mother of Prophet Muhammad"
+    },
+    {
+        word: "israh",
+        clue: "From Makkah to Jerusalem"
+    },
+    {
+        word: "kahba",
+        clue: "The holy box in Makkah"
+    },
+]
